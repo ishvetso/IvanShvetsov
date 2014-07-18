a16d4cfafab692681ffad80ae5bf16544e28cbe1
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2014-07-18 17:11:24 +0200

    other commit with major changes

--- HistPUBinsProducer/HistPUBinsProducer.cpp
+++ HistPUBinsProducer/HistPUBinsProducer.cpp
@@ -1,5 +1,7 @@
 #include "HistPUBinsProducer.hpp"
 
+typedef vector<bool> vbool;
+
 HistPUBinsProducer::HistPUBinsProducer()
 {}
 
@@ -14,7 +16,7 @@ void HistPUBinsProducer::SetPUBinsParameters(Int_t nPUmin_, Int_t nPUmax_, Int_t
   nPUmin = nPUmin_;
   interval = interval_;
   float x1 = (float)(nPUmax - nPUmin)/interval;
-  int x2 = (nPUmax - nPUmin)/interval;
+  int x2 = (nPUmax  - nPUmin)/interval;
   if ((x1-x2) != 0  ) 
   {
     cerr << "interval cannot be divided. The program supports the case when PU interval can be divided by integer. Please change the interval. Program closing.."<< endl;
@@ -29,55 +31,94 @@ void HistPUBinsProducer::SetBinParameters(Int_t Nbins_, Float_t xmin_, Float_t x
   xmax = xmax_;
 }
 
-vector <TH1F*> HistPUBinsProducer::ProduceHists()
+void HistPUBinsProducer::SetResponseParameters(Float_t xmin_response_, Float_t xmax_response_)
 {
-    TChain *chain = new TChain("pf");
+  xmin_response = xmin_response_;
+  xmax_response = xmax_response_;
+  
+}
+vector <TH1F*> HistPUBinsProducer::ProduceHists(string treename)
+{
+    TChain *chain = new TChain(treename.c_str());
+    TChain *chain_gen = new TChain("gen");
   
     for( unsigned int iFile = 0; iFile < FileNames.size(); ++iFile)
     {
       chain -> Add(FileNames.at(iFile).c_str());
+      chain_gen -> Add(FileNames.at(iFile).c_str());
     
     }
     
-     vector <float>* tmp = new std::vector<float>();
-     vector <int> * imatch_tmp = new std::vector<int>();
+    vector <float>* tmp = new std::vector<float>();
+    vector <float>* tmp_gen = new std::vector<float>();
+    vector <float>* pt = new std::vector<float>();
+    vector <int> * imatch_tmp = new std::vector<int>();
+    vector <bool> * isMatchedToBoson_tmp = new std::vector<bool>();
+    
     Int_t nPU;
+    
     chain -> SetBranchAddress(VarName.c_str(), &tmp);
+    chain_gen -> SetBranchAddress(VarName.c_str(), &tmp_gen); //corresponding gen info
     chain -> SetBranchAddress("npu", &nPU);
     chain -> SetBranchAddress("imatch", &imatch_tmp);
+    chain -> SetBranchAddress("is_MatchedToBoson", &isMatchedToBoson_tmp);
+    chain -> SetBranchAddress("pt", &pt);
     
     int nPUBins = (nPUmax- nPUmin)/interval;
     hists.reserve(nPUBins);
     hists.resize(nPUBins);
     
+    hists_response.reserve(nPUBins);
+    hists_response.resize(nPUBins);
     
+    //cout << nPUBins << endl;
     for (unsigned int iHist = 0; iHist < nPUBins; iHist++)
     {
       hists.at(iHist) = new TH1F( "", "", Nbins, xmin, xmax);
+      hists_response.at(iHist) = new TH1F( "", "", Nbins, xmin_response, xmax_response);
     }
     
+    
+    if ((chain -> GetEntriesFast())!= (chain_gen -> GetEntriesFast())) 
+    {
+      cout <<  "the number of entries in the gen tree is not equal to the number of entries in the " << treename << " tree " << endl;
+      exit(EXIT_FAILURE);
+    }
     for (unsigned int iEntry =0;  iEntry < chain -> GetEntriesFast(); ++iEntry)
     {
       chain -> GetEntry(iEntry);
-    //  cout << "---------------" << endl;
-     // cout << iEntry << endl;
-      if (nPU > nPUmax || nPU < nPUmin) continue;
+      chain_gen -> GetEntry(iEntry);
+      //cout << "gen size " <<  tmp_gen -> size() << endl;
+
+      if (nPU >= nPUmax || nPU < nPUmin) continue;
       int BinNumber;
       BinNumber = ((nPU - nPUmin)/interval ) + 1;
-      if (nPU == nPUmax) BinNumber--;
-      //cout << nPU << "  "<< BinNumber <<  endl;
-      
-      
-     
-      
+    //  if (nPU == nPUmax) BinNumber--;
+     // cout << BinNumber << "  " << nPU << endl;
+    //  cout << isMatchedToBoson_tmp -> size() << " " << pt -> size() << " " << tmp -> size() << endl;
+      //cout << "tmp size" << tmp -> size() << endl;
+      //cout << "imatch size" << imatch_tmp-> size() << endl;
       for (unsigned int iE =0; iE < tmp -> size(); ++iE)
       {
-	if (( imatch_tmp -> at(iE)) != -1 )hists.at(BinNumber -1 ) -> Fill(tmp -> at(iE));
-	
+	//cout << BinNumber -1 << endl;
+	if ((isMatchedToBoson_tmp -> at(iE)) && ((pt -> at(iE))> 300.) &&  imatch_tmp -> at(iE) != -1) 
+	{
+	  //cout << "imatch    "<< imatch_tmp -> at(iE) << endl;
+	  //if (imatch_tmp -> at(iE) == -1) cout << "!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+	  hists.at(BinNumber -1 ) -> Fill(tmp -> at(iE));
+	  hists_response.at(BinNumber -1 ) -> Fill(((tmp -> at(iE)) - (tmp_gen -> at(imatch_tmp -> at(iE)))) );
+	  
+	  
+	}
+	//cout << isMatchedToBoson_tmp -> at(iE) << endl;
+	//cout << "BinNumber = " << BinNumber <<  "  nPU = " <<  nPU << endl;
+		
       }
       
       tmp -> clear();
-      imatch_tmp -> clear();
+      isMatchedToBoson_tmp -> clear();
+      tmp_gen -> clear();
+      pt -> clear();
     }
      
      return hists;
--- HistPUBinsProducer/HistPUBinsProducer.hpp
+++ HistPUBinsProducer/HistPUBinsProducer.hpp
@@ -13,21 +13,26 @@
 #include <fstream>
 #include <vector>
 #include <stdlib.h>
+#include "TPaveText.h"
+
+using namespace std;
 
 class HistPUBinsProducer
 {
    vector <TFile> Files;
    vector <string> FileNames;
    Int_t nPUmax, nPUmin, interval, Nbins;
-   Float_t xmin, xmax;
+   Float_t xmin, xmax, xmin_response, xmax_response;
    vector <TH1F*> hists;
-   
+  
 
 public:
   string VarName;
+  vector <TH1F*> hists_response;
   HistPUBinsProducer();
    void SetInputFiles(string inputFile);
    void SetPUBinsParameters(Int_t nPUmin_, Int_t nPUmax_, Int_t interval_);
+   void SetResponseParameters(Float_t xmin_response_, Float_t xmax_response_);
    void SetBinParameters(Int_t Nbins_, Float_t xmin_, Float_t xmax_);
-   vector <TH1F*>  ProduceHists();
+   vector <TH1F*>  ProduceHists(string treename);
 };  
