c5dc6c7b69885d99de3df630773d514813887c00
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2014-08-05 15:16:25 +0200

    updating summary plot: there are 2 macros now

--- JetSubstructure_Studies/summary.cpp
+++ JetSubstructure_Studies/summary.cpp
@@ -2,6 +2,7 @@
 #include "TTree.h"
 #include "TChain.h"
 #include "TCanvas.h"
+#include "TGraphErrors.h"
 #include "TMultiGraph.h"
 #include "TLegend.h"
 #include "TLine.h"
@@ -17,7 +18,9 @@
 
 using namespace std;
 
-void summary(string inputFileList, vector <string> treeNames)
+typedef vector <string> vstring;
+
+void summary(vstring treeNames)
 {
   gStyle->SetOptStat(0);
   gStyle->SetOptTitle(0);
@@ -26,28 +29,20 @@ void summary(string inputFileList, vector <string> treeNames)
   Colors.push_back(kBlue);
   Colors.push_back(kGreen);
   Colors.push_back(kRed);
-  
-  vector <string> Labels;
-  
-  
-  TFile file_out("summary.root", "RECREATE");
- 
-  vector <TH1F * > hists_summary_Fit;
-  vector <TH1F * > hists_summary_Estimation;
-  
-  hists_summary_Fit.reserve(treeNames.size());
-  hists_summary_Estimation.reserve(treeNames.size());
-  
-  vector <string> VarName;
 
   
+  vector <string> TreeLabels;
+  
+  TFile file("summary.root", "READ");
+  vector <string> LabelsAxis;
+  
+  vector <TH1F *> HISTS_Fit, HISTS_RMS, HISTS_Fit_response, HISTS_RMS_response;
+  
+  vector <string> VarName;
   
   for (unsigned int iTree = 0; iTree < treeNames.size(); ++iTree )
   {
-    TChain chain((treeNames.at(iTree)).c_str());
-    TChain chain_gen("gen");
-    vector <TH1F*> hists;
-   
+    
     if (  (treeNames.at(iTree) == "pf" ) ||  (treeNames.at(iTree) == "chs" ) )
     {
         VarName.push_back("m");
@@ -84,188 +79,191 @@ void summary(string inputFileList, vector <string> treeNames)
     }
     
     else cerr << "tree name is not supported" << endl;
-    
-    std::ifstream ifs(inputFileList);
-    
-    std::string line;
 
-    string prefix = "root://eoscms.cern.ch/";
-    string filename;
-    
-    int counter = 0;
-    while(std::getline(ifs, line))
-    {
-	filename = prefix + line;
-	cout << filename << endl;
-	chain.Add(filename.c_str());
-	chain_gen.Add(filename.c_str());
-	counter ++;
-	//if (counter > 1) break;
-	
-    }
-        
-    vector< vector <float>*> tmp;
-    vector< vector <float>*> tmp_gen;
-    vector <int> * imatch_tmp = new std::vector<int>();
-    vector <float>* pt = new std::vector<float>();
-    vector <bool> * isMatchedToBoson_tmp = new std::vector<bool>();
-      
-    
-    tmp.reserve(VarName.size());
-    tmp_gen.reserve(VarName.size());
+
      
-    vector <string> LabelsAxis;
+    
     
     for (unsigned int iVar = 0; iVar < VarName.size(); iVar++)
     {
-      vector <float>* a = new std::vector<float>();
-      vector <float>* b = new std::vector<float>();
-      tmp.push_back(a);
-      tmp_gen.push_back(b);
-      TH1F *hist_tmp ;
-      hist_tmp = new TH1F((VarName.at(iVar) + "_" + treeNames.at(iTree)).c_str(), (VarName.at(iVar) + "_" + treeNames.at(iTree)).c_str(), 100, -150., 150.);
-      hists.push_back(hist_tmp); 
-      
-      if (VarName.at(iVar) == "m") LabelsAxis.push_back("m");
-      else if (VarName.at(iVar) == "mraw") LabelsAxis.push_back("m_{raw}");
-      else if ((VarName.at(iVar) == "mpruned_zcut_010_R_cut_050") || (VarName.at(iVar) == "mprunedsafe_zcut_010_R_cut_050") ) LabelsAxis.push_back("m_{pruned} z_{cut}=0.1 R_{cut}=0.5");
-      else if ((VarName.at(iVar) == "mpruned_zcut_005_R_cut_050") || (VarName.at(iVar) == "mprunedsafe_zcut_005_R_cut_050")) LabelsAxis.push_back("m_{pruned} z_{cut}=0.05 R_{cut}=0.5");
-      else if ((VarName.at(iVar) == "mpruned_zcut_005_R_cut_075") || (VarName.at(iVar) == "mprunedsafe_zcut_005_R_cut_075")) LabelsAxis.push_back("m_{pruned} z_{cut}=0.05 R_{cut}=0.75");
-      else if ((VarName.at(iVar) == "mpruned_zcut_010_R_cut_075") || (VarName.at(iVar) == "mprunedsafe_zcut_010_R_cut_075")) LabelsAxis.push_back("m_{pruned} z_{cut}=0.1 R_{cut}=0.75");
-      else if ((VarName.at(iVar) == "mtrim_Rtrim_020_Ptfrac_005") || (VarName.at(iVar) == "mtrimsafe_Rtrim_020_Ptfrac_005")) LabelsAxis.push_back("m_{trim} R_{trim}=0.2 pT_{frac}=0.05");
-      else if ((VarName.at(iVar) == "mtrim_Rtrim_010_Ptfrac_003") || (VarName.at(iVar) == "mtrimsafe_Rtrim_010_Ptfrac_003")) LabelsAxis.push_back("m_{trim} R_{trim}=0.1 pT_{frac}=0.03");
-      else if ((VarName.at(iVar) == "mtrim_Rtrim_020_Ptfrac_003") || (VarName.at(iVar) == "mtrimsafe_Rtrim_020_Ptfrac_003")) LabelsAxis.push_back("m_{trim} R_{trim}=0.2 pT_{frac}=0.03");
-      else if ((VarName.at(iVar) == "mtrim_Rtrim_030_Ptfrac_003") || (VarName.at(iVar) == "mtrimsafe_Rtrim_030_Ptfrac_003")) LabelsAxis.push_back("m_{trim} R_{trim}=0.3 pT_{frac}=0.03");
-      else if ((VarName.at(iVar) == "msoftdrop_beta10") || (VarName.at(iVar) == "msoftdropsafe_beta10")) LabelsAxis.push_back("m_{softdrop} #beta=1");
-      else if ((VarName.at(iVar) == "msoftdrop_beta00") || (VarName.at(iVar) == "msoftdropsafe_beta00")) LabelsAxis.push_back("m_{softdrop} #beta=0");
-      else if ((VarName.at(iVar) == "msoftdrop_beta20") || (VarName.at(iVar) == "msoftdropsafe_beta20")) LabelsAxis.push_back("m_{softdrop} #beta=2");
-      else cout << "Not supported label " << endl;	
-    }
-    
-    chain.SetBranchAddress("imatch", &imatch_tmp);
-    chain.SetBranchAddress("pt", &pt);
-    chain.SetBranchAddress("is_MatchedToBoson", &isMatchedToBoson_tmp);
-    
-    for (unsigned int iVar = 0; iVar < tmp.size(); iVar++)
-    {
-      (tmp.at(iVar)) -> clear();
-      (tmp_gen.at(iVar)) -> clear();
-      chain.SetBranchAddress((VarName.at(iVar)).c_str(), &tmp.at(iVar));
-      chain_gen.SetBranchAddress((VarName.at(iVar)).c_str(), &tmp_gen.at(iVar));
-    
-    }
-
-    cout << chain.GetEntries() << " "  << chain_gen.GetEntries() << endl;  
-
-    for (unsigned int i = 0; i < chain.GetEntries(); ++i)
-    {
-      
-      chain.GetEntry(i);
-      chain_gen.GetEntry(i);
-    
-      
-      for (unsigned int iVar = 0; iVar < VarName.size(); ++iVar )
+         
+      if (iTree == 0)
       {
-	for (unsigned int iE = 0; iE < (tmp.at(iVar)) -> size(); ++iE )
-	{
-	  if((isMatchedToBoson_tmp -> at(iE) == true) && ((pt -> at(iE))> 300.) &&  imatch_tmp -> at(iE) != -1) 
-	  {  
-	    hists.at(iVar) -> Fill( (((tmp.at(iVar)) -> at(iE)) - ((tmp_gen.at(iVar)) -> at(imatch_tmp -> at(iE)))) );
-	  }
-	}
-	  
-	(tmp.at(iVar)) -> clear();
-	(tmp_gen.at(iVar)) -> clear();
+	if (VarName.at(iVar) == "m") LabelsAxis.push_back("m");
+	else if (VarName.at(iVar) == "mraw") LabelsAxis.push_back("m_{raw}");
+	else if ((VarName.at(iVar) == "mpruned_zcut_010_R_cut_050") || (VarName.at(iVar) == "mprunedsafe_zcut_010_R_cut_050") ) LabelsAxis.push_back("m_{pruned} z_{cut}=0.1 R_{cut}=0.5");
+	else if ((VarName.at(iVar) == "mpruned_zcut_005_R_cut_050") || (VarName.at(iVar) == "mprunedsafe_zcut_005_R_cut_050")) LabelsAxis.push_back("m_{pruned} z_{cut}=0.05 R_{cut}=0.5");
+	else if ((VarName.at(iVar) == "mpruned_zcut_005_R_cut_075") || (VarName.at(iVar) == "mprunedsafe_zcut_005_R_cut_075")) LabelsAxis.push_back("m_{pruned} z_{cut}=0.05 R_{cut}=0.75");
+	else if ((VarName.at(iVar) == "mpruned_zcut_010_R_cut_075") || (VarName.at(iVar) == "mprunedsafe_zcut_010_R_cut_075")) LabelsAxis.push_back("m_{pruned} z_{cut}=0.1 R_{cut}=0.75");
+	else if ((VarName.at(iVar) == "mtrim_Rtrim_020_Ptfrac_005") || (VarName.at(iVar) == "mtrimsafe_Rtrim_020_Ptfrac_005")) LabelsAxis.push_back("m_{trim} R_{trim}=0.2 pT_{frac}=0.05");
+	else if ((VarName.at(iVar) == "mtrim_Rtrim_010_Ptfrac_003") || (VarName.at(iVar) == "mtrimsafe_Rtrim_010_Ptfrac_003")) LabelsAxis.push_back("m_{trim} R_{trim}=0.1 pT_{frac}=0.03");
+	else if ((VarName.at(iVar) == "mtrim_Rtrim_020_Ptfrac_003") || (VarName.at(iVar) == "mtrimsafe_Rtrim_020_Ptfrac_003")) LabelsAxis.push_back("m_{trim} R_{trim}=0.2 pT_{frac}=0.03");
+	else if ((VarName.at(iVar) == "mtrim_Rtrim_030_Ptfrac_003") || (VarName.at(iVar) == "mtrimsafe_Rtrim_030_Ptfrac_003")) LabelsAxis.push_back("m_{trim} R_{trim}=0.3 pT_{frac}=0.03");
+	else if ((VarName.at(iVar) == "msoftdrop_beta10") || (VarName.at(iVar) == "msoftdropsafe_beta10")) LabelsAxis.push_back("m_{softdrop} #beta=1");
+	else if ((VarName.at(iVar) == "msoftdrop_beta00") || (VarName.at(iVar) == "msoftdropsafe_beta00")) LabelsAxis.push_back("m_{softdrop} #beta=0");
+	else if ((VarName.at(iVar) == "msoftdrop_beta20") || (VarName.at(iVar) == "msoftdropsafe_beta20")) LabelsAxis.push_back("m_{softdrop} #beta=2");
+	else cout << "Not supported label " << endl;	
       }
     }
     
-    TH1F * hist_Summary_Fit = new TH1F (("Summary_Fit" + treeNames.at(iTree)).c_str(), ("Summary_Fit" + treeNames.at(iTree)).c_str(), VarName.size(), 0., VarName.size());
-    TH1F * hist_Summary_Estimation = new TH1F (("Summary_Estimation" + treeNames.at(iTree)).c_str(), ("Summary_Estimation" + treeNames.at(iTree)).c_str(), VarName.size(), 0., VarName.size());
+
+    file.cd();
+    TH1F *hist_RMS = new TH1F(("RMS_" + treeNames.at(iTree)).c_str(), ( "RMS_" + treeNames.at(iTree)).c_str(), VarName.size(), 0., VarName.size());
+    TH1F *hist_Fit  = new TH1F(( "Fit_" + treeNames.at(iTree)).c_str(), ("Fit_" + treeNames.at(iTree)).c_str(), VarName.size(), 0., VarName.size());
     
-    hists_summary_Fit.push_back(hist_Summary_Fit);
-    hists_summary_Estimation.push_back(hist_Summary_Estimation);
+    TH1F *hist_RMS_response = new TH1F(("RMS_response_" + treeNames.at(iTree)).c_str(), ( "RMS_response_" + treeNames.at(iTree)).c_str(), VarName.size(), 0., VarName.size());
+    TH1F *hist_Fit_response  = new TH1F(( "Fit_response_" + treeNames.at(iTree)).c_str(), ("Fit_response_" + treeNames.at(iTree)).c_str(), VarName.size(), 0., VarName.size());
     
-    file_out.cd();
+    HISTS_Fit.push_back(hist_Fit);
+    HISTS_RMS.push_back(hist_RMS);
+    
+    HISTS_Fit_response.push_back(hist_Fit_response);
+    HISTS_RMS_response.push_back(hist_RMS_response);
+    
     
     for (unsigned int  iVar = 0; iVar < VarName.size(); iVar++ )
     {
-      hists.at(iVar) -> Fit("gaus");
-      TF1 * mass = (TF1*) hists.at(iVar) -> GetFunction("gaus");
+      TH1F *hist = (TH1F*)file.Get((VarName.at(iVar) + "_" + treeNames.at(iTree)).c_str());
+      TH1F *hist_response = (TH1F*)file.Get(("response_" +  VarName.at(iVar) + "_" + treeNames.at(iTree)).c_str());
+     
+      hist -> Fit("gaus");
+      TF1 * mass = (TF1*) hist -> GetFunction("gaus");
       
-      hists_summary_Fit.at(iTree) -> SetBinContent(iVar + 1, mass -> GetParameter(2));
-      hists_summary_Estimation.at(iTree) -> SetBinContent(iVar + 1, hists.at(iVar) -> GetRMS());
+      hist_response -> Fit("gaus");
+      TF1 * mass_response = (TF1*) hist_response -> GetFunction("gaus");
       
-      hists_summary_Fit.at(iTree) -> GetXaxis() -> SetBinLabel(iVar + 1, (LabelsAxis.at(iVar)).c_str());
-      hists.at(iVar)  -> Write();
+     // hist_response -> Fit("gaus");
+      //TF1 * mass_response = (TF1*) hist_response -> GetFunction("gaus");
+      HISTS_RMS.at(iTree) -> SetBinContent(iVar + 1, hist -> GetRMS());
+      HISTS_RMS.at(iTree) -> SetBinError(iVar + 1, hist -> GetRMSError());
+      HISTS_RMS.at(iTree)  -> GetXaxis() -> SetBinLabel(iVar + 1, (LabelsAxis.at(iVar)).c_str());
+      
+      HISTS_Fit.at(iTree) -> SetBinContent(iVar + 1, mass -> GetParameter(2));
+      HISTS_Fit.at(iTree) -> SetBinError(iVar + 1, mass -> GetParError(2));
+      HISTS_Fit.at(iTree) -> GetXaxis() -> SetBinLabel(iVar + 1, (LabelsAxis.at(iVar)).c_str());
+
+      HISTS_RMS_response.at(iTree) -> SetBinContent(iVar + 1, hist_response -> GetRMS());
+      HISTS_RMS_response.at(iTree) -> SetBinError(iVar + 1, hist_response -> GetRMSError());
+      HISTS_RMS_response.at(iTree)  -> GetXaxis() -> SetBinLabel(iVar + 1, (LabelsAxis.at(iVar)).c_str());
+      
+      HISTS_Fit_response.at(iTree) -> SetBinContent(iVar + 1, mass_response -> GetParameter(2));
+      HISTS_Fit_response.at(iTree) -> SetBinError(iVar + 1, mass_response -> GetParError(2));
+      HISTS_Fit_response.at(iTree) -> GetXaxis() -> SetBinLabel(iVar + 1, (LabelsAxis.at(iVar)).c_str());
+
+
+      
+      if ( (100*(( hist -> GetRMSError())/(hist -> GetRMS() ))) > 1.0 ) cerr << "@@@@@@@ Uncertainty is > 1 %" << endl;
+      if ( (100*(( mass -> GetParError(2))/(mass -> GetParameter(2) ))) > 1.0 ) cerr << "@@@@@@@ Uncertainty is > 1 %" << endl;            
+      
+      if ( (100*(( hist_response -> GetRMSError())/(hist_response -> GetRMS() ))) > 1.0 ) cerr << "@@@@@@@ Uncertainty is > 1 %" << endl;
+      if ( (100*(( mass_response -> GetParError(2))/(mass_response -> GetParameter(2) ))) > 1.0 ) cerr << "@@@@@@@ Uncertainty is > 1 %" << endl;         
+    
       
     }
     
-    hists_summary_Fit.at(iTree) -> GetXaxis() -> SetLabelSize(0.03);
-    hists_summary_Fit.at(iTree) -> SetMarkerStyle(22);
-    hists_summary_Fit.at(iTree) -> SetMarkerSize(2.);
-    hists_summary_Fit.at(iTree) -> SetMarkerColor(Colors.at(iTree));
+    HISTS_RMS.at(iTree) -> GetXaxis() -> SetLabelSize(0.03);
+    HISTS_RMS.at(iTree) -> SetMarkerStyle(21);
+    HISTS_RMS.at(iTree) -> SetMarkerSize(2.0);
+    HISTS_RMS.at(iTree) -> SetMarkerColor(Colors.at(iTree));
+    HISTS_RMS.at(iTree) -> SetLineColor(Colors.at(iTree));
+    HISTS_RMS.at(iTree) -> SetLineWidth(2.);
     
-    hists_summary_Estimation.at(iTree) -> GetXaxis() -> SetLabelSize(0.05);
-    hists_summary_Estimation.at(iTree) -> SetMarkerStyle(21);
-    hists_summary_Estimation.at(iTree) -> SetMarkerSize(2.);
-    hists_summary_Estimation.at(iTree) -> SetMarkerColor(Colors.at(iTree));
-   
-        
-    hists.clear();
-    LabelsAxis.clear();
+    HISTS_Fit.at(iTree) -> GetXaxis() -> SetLabelSize(0.03);
+    HISTS_Fit.at(iTree) -> SetMarkerStyle(22);
+    HISTS_Fit.at(iTree) -> SetMarkerSize(2.0);
+    HISTS_Fit.at(iTree) -> SetMarkerColor(Colors.at(iTree));
+    HISTS_Fit.at(iTree) -> SetLineColor(Colors.at(iTree));
+    HISTS_Fit.at(iTree) -> SetLineWidth(5.);
+
     
-    if (treeNames.at(iTree) == "pf") Labels.push_back("PF Jets");
-    else if (treeNames.at(iTree) == "chs") Labels.push_back("PF + CHS Jets");
-    else if (treeNames.at(iTree) == "puppi") Labels.push_back("PUPPI Jets");
+    HISTS_RMS_response.at(iTree) -> GetXaxis() -> SetLabelSize(0.03);
+    HISTS_RMS_response.at(iTree) -> SetMarkerStyle(21);
+    HISTS_RMS_response.at(iTree) -> SetMarkerSize(2.0);
+    HISTS_RMS_response.at(iTree) -> SetMarkerColor(Colors.at(iTree));
+    HISTS_RMS_response.at(iTree) -> SetLineColor(Colors.at(iTree));
+    HISTS_RMS_response.at(iTree) -> SetLineWidth(2.);
+    
+    HISTS_Fit_response.at(iTree) -> GetXaxis() -> SetLabelSize(0.03);
+    HISTS_Fit_response.at(iTree) -> SetMarkerStyle(22);
+    HISTS_Fit_response.at(iTree) -> SetMarkerSize(2.0);
+    HISTS_Fit_response.at(iTree) -> SetMarkerColor(Colors.at(iTree));
+    HISTS_Fit_response.at(iTree) -> SetLineColor(Colors.at(iTree));
+    HISTS_Fit_response.at(iTree) -> SetLineWidth(5.);
    
-   
+    if (treeNames.at(iTree) == "pf") TreeLabels.push_back("PF Jets");
+    else if (treeNames.at(iTree) == "chs") TreeLabels.push_back("PF + CHS Jets");
+    else if (treeNames.at(iTree) == "puppi") TreeLabels.push_back("PUPPI Jets");
+    
     VarName.clear();
   }
   
-  TCanvas *c1=  new TCanvas("c1","canvas",1000,800);
-  c1->SetTickx(1);
-  c1->SetTicky(1); 
+   TCanvas *c1=  new TCanvas("c2","canvas",1000,800);
+  
+  c1 -> Clear();  
+  c1 -> SetTickx(1);
+  c1 -> SetTicky(1); 
   
   TPaveText descriptionPave(0.12,0.75,0.3,0.85, "NDC");
-  TPaveText descriptionPaveCMS(0.12,0.9,0.3,1.0, "NDC");
+  TPaveText descriptionPaveCMS(0.12,0.87,0.3,0.95, "NDC");
   descriptionPave.SetTextAlign(11);
   descriptionPave.SetFillStyle(0);  // transparent
   descriptionPave.SetBorderSize(0);
   descriptionPave.SetTextSize(0.03);
-  descriptionPave.AddText("WW, Antikt jets with R = 0.8");// \\ p_{T}^{jet} > 300 GeV");
+  descriptionPave.AddText("WW, Antikt jets with R = 0.8");
+  descriptionPave.AddText("");
   descriptionPave.AddText("p_{T}^{jet} > 300 GeV" );
-  descriptionPave.SetTextSize(0.025);
-  
+    
   descriptionPaveCMS.SetTextAlign(11);
   descriptionPaveCMS.SetFillStyle(0);  // transparent
   descriptionPaveCMS.SetBorderSize(0);
   descriptionPaveCMS.SetTextSize(0.03);
   descriptionPaveCMS.AddText("CMS Simulation Preliminary #sqrt{s} = 13 TeV");
   
-  TLegend  *leg = new TLegend(0.4,0.65,0.89,0.85);
-  leg ->  SetTextSize(0.023);
+  TLegend  *leg = new TLegend(0.65,0.69,0.85,0.89);
+  leg ->  SetTextSize(0.03);
   leg ->  SetFillStyle(0.);
   leg -> SetLineWidth(0.);
   leg -> SetLineColor(kWhite);
-  leg-> SetNColumns(2);
-
+  
   for (unsigned int iTree = 0; iTree < treeNames.size(); iTree++)
   {
-    hists_summary_Fit.at(iTree) -> GetYaxis() -> SetRangeUser(0., 65.);
-    hists_summary_Fit.at(iTree) -> Draw("PSAME");
-    hists_summary_Estimation.at(iTree) -> Draw("PSAME");
+    HISTS_Fit.at(iTree) -> GetYaxis() -> SetRangeUser(0., 55.);
+    HISTS_Fit.at(iTree) -> GetYaxis() -> SetTitle("Resolution");
+    HISTS_Fit.at(iTree) -> Draw("PHISTSAME");
+    HISTS_RMS.at(iTree) -> Draw("PHISTSAME");
     
-     leg->AddEntry(hists_summary_Estimation.at(iTree),(Labels.at(iTree) + " Estimation" ).c_str(),"p");
-     leg->AddEntry(hists_summary_Fit.at(iTree),(Labels.at(iTree) + " Fit Result " ).c_str(),"p");
+    leg->AddEntry(HISTS_Fit.at(iTree),(TreeLabels.at(iTree)).c_str(),"l");
+    
   }
+  
+  TLegend  *leg2 = new TLegend(0.5,0.69,0.6,0.89);
+  leg2 ->  SetTextSize(0.04);
+  leg2 ->  SetFillStyle(0.);
+  leg2 -> SetLineColor(kWhite);
+    
+  TGraph *gr1 = new TGraph();
+  TGraph *gr2 = new TGraph();
+
+  gr2 -> SetMarkerStyle(21);
+  gr1 -> SetMarkerStyle(22);
+
+  gr2 -> SetMarkerSize(2);
+  gr1 -> SetMarkerSize(2.);
+  
+  
+
+  leg2->AddEntry(gr1,"#sigma","p");
+  leg2->AddEntry(gr2,"RMS","p");
+
   
   descriptionPave.Draw();
   descriptionPaveCMS.Draw();
   leg -> Draw();
+  leg2 -> Draw();
   
- TLine *l1 = new TLine(2.,0.0,2.,44.0);
- TLine *l2 = new TLine(6.,0.0,6.,44.0);
- TLine *l3 = new TLine(10.,0.0,10.,44.0);
+ TLine *l1 = new TLine(2.,0.0,2.,38.);
+ TLine *l2 = new TLine(6.,0.0,6.,38.);
+ TLine *l3 = new TLine(10.,0.0,10.,38.);
  
  l1 -> SetLineColor(kYellow);
  l1 -> SetLineWidth(3.);
@@ -284,7 +282,30 @@ void summary(string inputFileList, vector <string> treeNames)
  l3 -> Draw("SAME");
   
   c1 -> SaveAs("summary_grooming_algos.png");
-   
+  c1 -> SaveAs("summary_grooming_algos.pdf");
+  
+   TCanvas *c3=  new TCanvas("c3","c3",1000,800);
+    c3 -> SetTickx(1);
+    c3 -> SetTicky(1); 
+   c3 -> cd();
+  for (unsigned int iTree = 0; iTree < treeNames.size(); iTree++)
+  {
+    HISTS_Fit_response.at(iTree) -> GetYaxis() -> SetRangeUser(0., 55.);
+    HISTS_Fit_response.at(iTree) -> GetYaxis() -> SetTitle("Resolution");
+    HISTS_Fit_response.at(iTree) -> Draw("PHISTSAME");
+    HISTS_RMS_response.at(iTree) -> Draw("PHISTSAME");
+      
+  }
+    
+   l1 -> Draw("SAME");
+   l2 -> Draw("SAME");
+   l3 -> Draw("SAME");  
+   descriptionPave.Draw();
+   descriptionPaveCMS.Draw();
+   leg -> Draw();
+   leg2 -> Draw();
+
+    c3 -> SaveAs("summary_grooming_algos_response.png");
 }
 
 int main(int argc, char** argv)
@@ -294,6 +315,6 @@ int main(int argc, char** argv)
     treeNames.push_back("chs");
     treeNames.push_back("puppi");
         
-    summary("/storage/a/ishvetso/git_repository/IvanShvetsov/scripts/RSGravToWW_kMpl01_M-1000_Tune4C_13TeV-pythia8_Spring14.txt", treeNames);
+    summary( treeNames);
   
 }
