fe2ee7efc75ccd76ccf367010a3c0e4c6ebf735d
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2017-01-18 17:34:07 +0100

    add script to remove text from pdf

---
+++ scripts/removeText.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# function removes a specified string (argument $2) with a new string (argument $3) in a given file (argument $2)
+
+ReplaceStringInFile(){
+ fileName=$1
+ filenameNoFormat=${fileName%.*}
+ pdftk $fileName output $filenameNoFormat"-compressed.pdf" uncompress
+ #modify
+ sed 's/'$2'/'$3'/g' < $filenameNoFormat"-compressed.pdf" > $filenameNoFormat"-new.pdf"
+ #compress
+ pdftk $filenameNoFormat"-new.pdf" output $fileName compress
+ rm $filenameNoFormat"-compressed.pdf" $filenameNoFormat"-new.pdf"
+}
+
+stringToReplace=$1
+ToReplace=$(echo $stringToReplace| tr " " "\n")
+
+for iFile in *.pdf
+do 
+  echo $iFile
+  for iToReplace in $ToReplace
+  do
+    ReplaceStringInFile $iFile $iToReplace $2
+  done
+done
+
